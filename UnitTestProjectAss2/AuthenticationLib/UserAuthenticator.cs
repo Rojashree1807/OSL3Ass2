ee5f7a6014fe5184bd9a0bb4ec24adfaf26fd5ca
Rojashree1807 <rojarojashree1807@gmail.com>
2024-01-08 17:18:45 +0530

    Initial commit

---
+++ UnitTestProjectAss2/AuthenticationLib/UserAuthenticator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationLib
+{
+    public class UserAuthenticator
+    {
+        private Dictionary<string, string> userDatabase = new Dictionary<string, string>();
+
+        public bool RegisterUser(string username, string password)
+        {
+            if (!userDatabase.ContainsKey(username))
+            {
+                userDatabase.Add(username, password);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool LoginUser(string username, string password)
+        {
+            if (userDatabase.TryGetValue(username, out string storedPassword))
+            {
+                return password == storedPassword;
+            }
+
+            return false;
+        }
+
+       
+    }
+}
---
+++ UnitTestProjectAss2/UnitTestProjectAss2/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using AuthenticationLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+
+namespace UnitTestProjectAss2
+{
+    [TestFixture]
+    public class UserAuthenticatorTests
+    {
+        private UserAuthenticator _userAuthenticator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userAuthenticator = new UserAuthenticator();
+        }
+
+        [Test]
+        public void RegisterUser_ValidCredentials_ReturnsTrue()
+        {
+
+            string username = "Rojashree";
+            string password = "password123";
+            bool result = _userAuthenticator.RegisterUser(username, password);
+
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void LoginUser_ValidCredentials_ReturnsTrue()
+        {
+
+            string username = "Rojashree";
+            string password = "password123";
+            _userAuthenticator.RegisterUser(username, password);
+            bool result = _userAuthenticator.LoginUser(username, password);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
